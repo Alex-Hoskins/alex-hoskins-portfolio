cc72296b7eff9a5e3cd4af3e1f8421f3d32701b8
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-11-12 15:44:46 -0800

    styling on picture and in navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom";
 import './Navigation.css'
 
 const Navigation = () =>{
+    const title = '<Alex-Hoskins/>'
     return(
         <nav className='navigation'>
             <div className='nav-box'>
                 <Link className='links' to='/'><span>Home</span></Link>
                 <Link className='links' to='/projects'><span>Projects</span></Link>
                 <Link className='links' to='/'><span>Contact</span></Link>
+            </div>
+            <div className='navTitle'>
+                <p>{title}</p>
             </div>
             <div className='nav-box'>
                 <a href="https://www.linkedin.com/in/alex-hoskins-dev" target='_blank'><img src="https://www.pinclipart.com/picdir/big/574-5743993_linkedin-icon-white-png-clipart.png" width="25" alt="linkedin logo black png" /></a>
