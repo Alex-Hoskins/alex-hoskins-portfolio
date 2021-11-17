b5e2382ebc77b40375c44133471fee2c07b4697c
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-11-17 13:56:08 -0800

    home page images uploaded

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,8 @@ const Home =()=>{
         <div class="homeImg">
           <img src="https://lh3.googleusercontent.com/pw/AM-JKLUUOtoXWPYzd-umzXj3jshAZo6G7J5TFRZ3wdh6g7UK8fp5LDCm4jf7zus_fHO9Fhd2FkJAdBFmxOBNqdqC4h6R7id0ej7swxkGCUCEZ9PZOe_6bv10IW9dcDFNZsDNWwmGOigP_FHsx6u8qBbRwGU=w1065-h710-no?authuser=0"  alt="" width="300" height="300" />
         </div>
+        <h1>Web Developer & Software Engineer</h1>
+        <img className='bridgeImg' src="https://lh3.googleusercontent.com/4Vq_EYfeobEke5L_HkQQHza3R5X1oxIgCIfwhoZtt4TbBtSijQ5UJhmnhBuyiad2VBYSfXMgJ19ecT1TT_0f3zaarBIk8a937_87yE33HaGjI-zRv7o4CTBBrfpWyYG9CJ47Td5B=w2400" alt="hello" />
        </div>
        
     )
