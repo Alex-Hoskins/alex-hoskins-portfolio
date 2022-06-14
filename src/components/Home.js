553d8c620c40ddfe62d20fae5741187e574ff2da
Alex Hoskins <alexander.hoskins@yahoo.com>
2022-06-14 15:06:03 -0700

    home design changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,11 @@ const Home =()=>{
     return(
       <div>
        <div className="home">
-        <div class="homeImg">
+        {/* <div class="homeImg">
           <img src="https://lh3.googleusercontent.com/pw/AM-JKLUUOtoXWPYzd-umzXj3jshAZo6G7J5TFRZ3wdh6g7UK8fp5LDCm4jf7zus_fHO9Fhd2FkJAdBFmxOBNqdqC4h6R7id0ej7swxkGCUCEZ9PZOe_6bv10IW9dcDFNZsDNWwmGOigP_FHsx6u8qBbRwGU=w1065-h710-no?authuser=0"  alt="" width="300" height="300" />
-        </div>
+        </div> */}
         <div class="wrapper">
-          <div class="static-txt">I'm a</div>
+          <div class="static-txt"></div>
           <ul class="dynamic-txts">
             <li><span>Developer</span></li>
             <li><span>Innovator</span></li>
