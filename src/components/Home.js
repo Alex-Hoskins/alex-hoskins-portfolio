93a0a07eec4ba54c5aa0b0c48c91e8f69677fbe2
Alex Hoskins <alexander.hoskins@yahoo.com>
2022-06-14 14:55:18 -0700

    Changed styling and background image of home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import './Home.css';
 const Home =()=>{
     let title = '<Alex-Hoskins/>'
     return(
+      <div>
        <div className="home">
         <div class="homeImg">
           <img src="https://lh3.googleusercontent.com/pw/AM-JKLUUOtoXWPYzd-umzXj3jshAZo6G7J5TFRZ3wdh6g7UK8fp5LDCm4jf7zus_fHO9Fhd2FkJAdBFmxOBNqdqC4h6R7id0ej7swxkGCUCEZ9PZOe_6bv10IW9dcDFNZsDNWwmGOigP_FHsx6u8qBbRwGU=w1065-h710-no?authuser=0"  alt="" width="300" height="300" />
@@ -21,6 +22,7 @@ const Home =()=>{
         </div>
         <h1>{title}</h1>
         <img className='bridgeImg' src="https://lh3.googleusercontent.com/4Vq_EYfeobEke5L_HkQQHza3R5X1oxIgCIfwhoZtt4TbBtSijQ5UJhmnhBuyiad2VBYSfXMgJ19ecT1TT_0f3zaarBIk8a937_87yE33HaGjI-zRv7o4CTBBrfpWyYG9CJ47Td5B=w2400" alt="black bridge" />
+        </div>
         <About/>
        </div>
        
